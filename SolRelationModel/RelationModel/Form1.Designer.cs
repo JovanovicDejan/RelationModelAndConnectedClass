8e5040ab355502e4e5b7d86ccbf7fc471ca3dcaa
Dejan Jovanovic <dejanjovanovicofficial@gmail.com>
2023-04-16 12:44:55 +0200

    Connected Class Crudu and Relation Model

---
+++ SolRelationModel/RelationModel/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace RelationModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvKupci = new System.Windows.Forms.DataGridView();
+            this.dgvFakture = new System.Windows.Forms.DataGridView();
+            this.dgvFaktureStavke = new System.Windows.Forms.DataGridView();
+            this.btnShowData = new System.Windows.Forms.Button();
+            this.btnCreateXML = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKupci)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFakture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFaktureStavke)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvKupci
+            // 
+            this.dgvKupci.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKupci.Location = new System.Drawing.Point(22, 236);
+            this.dgvKupci.Name = "dgvKupci";
+            this.dgvKupci.Size = new System.Drawing.Size(345, 385);
+            this.dgvKupci.TabIndex = 0;
+            // 
+            // dgvFakture
+            // 
+            this.dgvFakture.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFakture.Location = new System.Drawing.Point(461, 236);
+            this.dgvFakture.Name = "dgvFakture";
+            this.dgvFakture.Size = new System.Drawing.Size(343, 385);
+            this.dgvFakture.TabIndex = 1;
+            // 
+            // dgvFaktureStavke
+            // 
+            this.dgvFaktureStavke.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFaktureStavke.Location = new System.Drawing.Point(902, 236);
+            this.dgvFaktureStavke.Name = "dgvFaktureStavke";
+            this.dgvFaktureStavke.Size = new System.Drawing.Size(346, 385);
+            this.dgvFaktureStavke.TabIndex = 2;
+            // 
+            // btnShowData
+            // 
+            this.btnShowData.Location = new System.Drawing.Point(72, 39);
+            this.btnShowData.Name = "btnShowData";
+            this.btnShowData.Size = new System.Drawing.Size(161, 52);
+            this.btnShowData.TabIndex = 3;
+            this.btnShowData.Text = "Show Data";
+            this.btnShowData.UseVisualStyleBackColor = true;
+            this.btnShowData.Click += new System.EventHandler(this.btnShowData_Click);
+            // 
+            // btnCreateXML
+            // 
+            this.btnCreateXML.Location = new System.Drawing.Point(509, 39);
+            this.btnCreateXML.Name = "btnCreateXML";
+            this.btnCreateXML.Size = new System.Drawing.Size(206, 52);
+            this.btnCreateXML.TabIndex = 4;
+            this.btnCreateXML.Text = "Kreiraj XML Fajl";
+            this.btnCreateXML.UseVisualStyleBackColor = true;
+            this.btnCreateXML.Click += new System.EventHandler(this.btnCreateXML_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 691);
+            this.Controls.Add(this.btnCreateXML);
+            this.Controls.Add(this.btnShowData);
+            this.Controls.Add(this.dgvFaktureStavke);
+            this.Controls.Add(this.dgvFakture);
+            this.Controls.Add(this.dgvKupci);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKupci)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFakture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFaktureStavke)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvKupci;
+        private System.Windows.Forms.DataGridView dgvFakture;
+        private System.Windows.Forms.DataGridView dgvFaktureStavke;
+        private System.Windows.Forms.Button btnShowData;
+        private System.Windows.Forms.Button btnCreateXML;
+    }
+}
+
---
+++ SolRelationModel/RelationModel/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelationModel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable Kupci, Fakture, FaktureStavke;
+        DataSet fakturisanje;
+
+
+        private void btnShowData_Click(object sender, EventArgs e)
+        {
+
+
+            //Create table Kupci
+            Kupci = new DataTable("Kupci");
+
+            //Create columns for table Kupci
+            DataColumn kupacID = new DataColumn("KupacID")
+            {
+                DataType = typeof(int),
+                AutoIncrementSeed = 1,
+                AutoIncrementStep = 1,
+                AllowDBNull = false,
+            };
+
+            //Primarykey
+            Kupci.PrimaryKey = new DataColumn[] { Kupci.Columns["KupacID"] };
+
+            DataColumn nazivKupca = new DataColumn("NazivKupca")
+            {
+                DataType = typeof(string),
+                MaxLength = 50,
+                AllowDBNull = false
+            };
+
+            DataColumn adresa = new DataColumn("Adresa")
+            {
+                DataType = typeof(string),
+                MaxLength = 200,
+                AllowDBNull = true
+            };
+
+            Kupci.Columns.Add(kupacID);
+            Kupci.Columns.Add(nazivKupca);
+            Kupci.Columns.Add(adresa);
+
+            //Create Table Fakture
+            Fakture = new DataTable("Fakture");
+
+            //Create columns for table Fakture
+            DataColumn fakturaID = new DataColumn("FakturaID")
+            {
+                DataType = typeof(int),
+                AutoIncrementSeed = 1,
+                AutoIncrementStep = 1,
+                AllowDBNull = false
+            };
+
+            //Primarykey
+            Fakture.PrimaryKey = new DataColumn[] { Fakture.Columns["FakturaID"] };
+
+            DataColumn fkKupacID = new DataColumn("KupacID")
+            {
+                DataType = typeof(int),
+                AllowDBNull = false
+            };
+
+            DataColumn datum = new DataColumn("Datum")
+            {
+                DataType = typeof(DateTime),
+                AllowDBNull = false,
+                DefaultValue = DateTime.Now
+            };
+
+            Fakture.Columns.Add(fakturaID);
+            Fakture.Columns.Add(fkKupacID);
+            Fakture.Columns.Add(datum);
+
+            //Create Table FaktureStavke
+            FaktureStavke = new DataTable("FaktureStavke");
+
+
+            //Create columns for table FaktureStavke
+
+            DataColumn fkFakturaID = new DataColumn("FakturaID")
+            {
+                DataType = typeof(int),
+                AllowDBNull = false,
+                Unique = false
+            };
+
+            DataColumn nazivStavke = new DataColumn("NazivStavke")
+            {
+                DataType = typeof(string),
+                MaxLength = 40,
+                AllowDBNull = false
+            };
+
+            DataColumn cenaStavke = new DataColumn("CenaStavke")
+            {
+                DataType = typeof(decimal),
+                AllowDBNull = false
+
+            };
+
+            //Primarykey
+            FaktureStavke.PrimaryKey = new DataColumn[] { FaktureStavke.Columns["FakturaID"], FaktureStavke.Columns["NazivStavke"] };
+
+
+            //Add columns
+            FaktureStavke.Columns.Add(fkFakturaID);
+            FaktureStavke.Columns.Add(nazivStavke);
+            FaktureStavke.Columns.Add(cenaStavke);
+            //DataSet
+
+            //Create dataset for table
+            fakturisanje = new DataSet("Fakturisanje");
+            fakturisanje.Tables.Add(Kupci);
+            fakturisanje.Tables.Add(Fakture);
+            fakturisanje.Tables.Add(FaktureStavke);
+
+            //Relations
+            DataRelation drFakturisanje = new DataRelation("RelacijaKupciUplate", Kupci.Columns["KupacID"], Fakture.Columns["KupacID"], true);
+
+            DataRelation drFaktureStavke = new DataRelation("drFaktureStavke", Fakture.Columns["FakturaID"], FaktureStavke.Columns["FakturaID"], true);
+            
+            fakturisanje.Relations.Add(drFakturisanje);
+            fakturisanje.Relations.Add(drFaktureStavke);
+
+            //ForeignKey
+            ForeignKeyConstraint fkFakturisanje = (ForeignKeyConstraint)Fakture.Constraints["RelacijaKupciUplate"];
+            fkFakturisanje.UpdateRule = Rule.None;
+            fkFakturisanje.DeleteRule = Rule.None;
+
+            ForeignKeyConstraint fkFaktureStavke = (ForeignKeyConstraint)FaktureStavke.Constraints["drFaktureStavke"];
+            fkFaktureStavke.UpdateRule = Rule.None;
+            fkFaktureStavke.DeleteRule = Rule.None;
+
+            Kupci.Rows.Add(1, "test", "Test");
+            Kupci.Rows.Add(2, "test1", "Test1");
+            Kupci.Rows.Add(3, "test2", "Test2");
+            Kupci.Rows.Add(4, "test3", "Test3");
+            Kupci.Rows.Add(5, "test4", "Test4");
+
+
+            Fakture.Rows.Add(1, 1);
+            Fakture.Rows.Add(2, 2);
+            Fakture.Rows.Add(3, 3);
+            Fakture.Rows.Add(4, 4);
+
+            FaktureStavke.Rows.Add(1, "Stavka 1", 100);
+            FaktureStavke.Rows.Add(2, "Stavka 1", 100);
+            FaktureStavke.Rows.Add(3, "Stavka 1", 100);
+            FaktureStavke.Rows.Add(4, "Stavka 1", 100);
+
+            dgvKupci.DataSource = Kupci;
+            dgvFakture.DataSource = Fakture;
+            dgvFaktureStavke.DataSource = FaktureStavke;
+
+        }
+        private void btnCreateXML_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                fakturisanje.WriteXml(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Fakturisano.xsd", XmlWriteMode.WriteSchema);
+                MessageBox.Show("Saved");
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
